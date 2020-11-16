acd8f6ddfcf71986ca79bc5168b8d35bb38fa529
Chika0IQ <mohdkhairin62@gmail.com>
2020-11-16 21:43:47 +0800

    Changes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Animator PlayerAnim;
-    float speed = 10.0f;
-    bool onCube = false;
-
+    float speed = 2.0f;
+    float health = 100f;
+    bool isDead = false;
 
     public GameObject Cube;
+    public GameObject HealthText;
     Rigidbody player;
-
-
+    Animator PlayerAnim;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,29 +24,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        // Move Forward Bool
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            PlayerAnim.SetBool("isStrafe", true);
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            PlayerAnim.SetBool("isStrafe", false);
-        }
-
-        //Attack Trigger
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            PlayerAttack();
-        }
-
-        if (onCube == true)
-        {
-            
-            PlayerAnim.SetBool("isCol", true);
-        }
+        GamePlay(); // Start of Gameplay
     }
 
     void PlayerAttack()
@@ -55,10 +34,79 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Decrease Health when player touches the cube by -10
         if (collision.gameObject.CompareTag("Cube"))
         {
-            PlayerAnim.SetBool("isCol", true);
-            onCube = true;
+            health -= 10;
+            HealthText.GetComponent<Text>().text = ("Health: " + health);
+        }
+    }
+
+    private void GamePlay()
+    {
+        bool iscol = PlayerAnim.GetBool("isCol");
+        if (!isDead)
+        {
+            //Forward Movememt
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                PlayerAnim.SetBool("isStrafe", true);
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                PlayerAnim.SetBool("isStrafe", false);
+            }
+
+            //Backward Movement
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                PlayerAnim.SetBool("isStrafe", true);
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                PlayerAnim.SetBool("isStrafe", false);
+            }
+
+            //Left Movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 270, 0);
+                PlayerAnim.SetBool("isStrafe", true);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                PlayerAnim.SetBool("isStrafe", false);
+            }
+
+            //Right Movement
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                PlayerAnim.SetBool("isStrafe", true);
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                PlayerAnim.SetBool("isStrafe", false);
+            }
+
+            // Call Attack Trigger
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayerAttack();
+            }
+
+            // Health of Player 
+            if (health <= 0)
+            {
+                PlayerAnim.SetBool("isCol", true);
+                isDead = true;
+            }
         }
     }
 }
